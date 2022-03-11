a87e03dc30ccc01a814e914e92f32d3f03fff337
AxisZql <mifei23333@outlook.com>
2022-03-11 11:22:10 +0800

    feat:add context to record all information of the current HTTP session

---
+++ context.go
@@ -0,0 +1,80 @@
+package axisapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// H is a shortcut for map[string]interface{}
+type H map[string]interface{}
+
+// Context is a shortcut for record all information of the current HTTP session
+// It's the most important part for axisapi
+type Context struct {
+	// origin object
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	// response info
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (ctx *Context) PostForm(key string) string {
+	return ctx.Req.FormValue(key)
+}
+
+func (ctx *Context) Query(key string) string {
+	return ctx.Req.URL.Query().Get(key)
+}
+
+// Status is used to set the HTTP response code
+func (ctx *Context) Status(code int) {
+	ctx.StatusCode = code
+	ctx.Writer.WriteHeader(code)
+}
+
+func (ctx *Context) SetHeader(key string, value string) {
+	ctx.Writer.Header().Set(key, value)
+}
+
+//--------------------------
+//	Setting up different formats HTTP Responses
+//--------------------------
+
+func (ctx *Context) String(code int, format string, value ...interface{}) {
+	ctx.SetHeader("Content-Type", "text/plain")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(fmt.Sprintf(format, value...)))
+}
+
+func (ctx *Context) JSON(code int, obj interface{}) {
+	ctx.SetHeader("Content-Type", "application/json")
+	ctx.Status(code)
+	encoder := json.NewEncoder(ctx.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(ctx.Writer, err.Error(), 500)
+	}
+}
+
+func (ctx *Context) Data(code int, data []byte) {
+	ctx.Status(code)
+	ctx.Writer.Write(data)
+}
+
+func (ctx *Context) HTML(code int, html string) {
+	ctx.SetHeader("Content-Type", "text/html")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(html))
+}
